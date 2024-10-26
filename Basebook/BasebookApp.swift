17338e0608d81f25a956c343073a9fdbed9e02f3
MZIO <mziodev@gmail.com>
2024-10-26 11:42:41 +0200

    Initial Commit

---
+++ Basebook/BasebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasebookApp.swift
+//  Basebook
+//
+//  Created by MZiO on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BasebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
